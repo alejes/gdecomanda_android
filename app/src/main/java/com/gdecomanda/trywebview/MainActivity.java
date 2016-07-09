e61a94129213654584ffd6d875de28c21758d17d
Rinchin <gomboev.rinchin@gmail.com>
2016-07-09 23:28:52 +0300

    Add Skeleton for Android Application

---
+++ app/src/main/java/com/gdecomanda/trywebview/MainActivity.java
@@ -0,0 +1,24 @@
+package com.gdecomanda.trywebview;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String url = "http://40.118.61.147/";
+        WebView view1 = (WebView)findViewById(R.id.webView);
+
+
+        view1.getSettings().setJavaScriptEnabled(true);
+        view1.loadUrl(url);
+    }
+}
